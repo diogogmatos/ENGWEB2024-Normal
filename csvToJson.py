e50bc3306308c2d195e9b4d2c19cf40953d2268c
Diogo Matos <diogomatosemail@gmail.com>
2024-05-16 17:16:16 +0100

    Update csvToJson.py

--- csvToJson.py
+++ csvToJson.py
@@ -6,11 +6,6 @@ def csv_to_json(csv_file, json_file):
     with open(csv_file, 'r') as file:
         csv_reader = csv.DictReader(file, delimiter=";")
         data = list(csv_reader)
-        
-        # Replace single-quote with three semicolons in the first column
-        for row in data:
-            if row.get('Column1') == "'":
-                row['Column1'] = ';;;'  # Replace single-quote with three semicolons
     
     # Write the data to a JSON file
     with open(json_file, 'w') as file:
@@ -18,5 +13,5 @@ def csv_to_json(csv_file, json_file):
 
 # Example usage:
 csv_file = 'contratos2024.csv'
-json_file = 'contratos2024_teste.json'
+json_file = 'contratos2024.json'
 csv_to_json(csv_file, json_file)
